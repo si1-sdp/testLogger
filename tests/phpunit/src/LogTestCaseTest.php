80e170e5562eae10b29809c550564782d99bd268
jm.gervais <jean-marie.gervais@dgfip.finances.gouv.fr>
2022-10-24 18:47:17 +0200

    improve tests

---
+++ tests/phpunit/src/LogTestCaseTest.php
@@ -0,0 +1,143 @@
+<?php
+/*
+ * This file is part of dgfip-si1\test-logger.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace DgfipSI1\testLoggerTest;
+
+use DgfipSI1\testLogger\LogTestCase;
+use DgfipSI1\testLogger\TestLogger;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+/**
+ * logTestCase tests
+ *
+ * @uses \DgfipSI1\testLogger\TestLogger
+ */
+class LogTestCaseTest extends TestCase
+{
+    /** @var LogTestCase */
+    protected $test;
+
+    /** @var TestLogger */
+    protected $logger;
+
+    /**
+     * @inheritDoc
+     *
+     * @return void
+     */
+    public function setup(): void
+    {
+        $this->test = new LogTestCase();
+        $class = new ReflectionClass(LogTestCase::class);
+
+        $lg = $class->getProperty('logger');
+        $lg->setAccessible(true);
+
+        $this->logger = new TestLogger();
+        $lg->setValue($this->test, $this->logger);
+    }
+
+   /**
+     * Test __call method
+     *
+     * @covers \DgfipSI1\testLogger\LogTestCase
+     */
+    public function testCallFunction(): void
+    {
+        $this->expectExceptionMessageMatches("/Call to undefined method/");
+        /** @phpstan-ignore-next-line */
+        $this->test->foo();
+    }
+   /**
+     * Test __call method and assertInLog
+     *
+     * @covers \DgfipSI1\testLogger\LogTestCase::__call
+     * @covers \DgfipSI1\testLogger\LogTestCase::assertInLog
+     *
+     */
+    public function testAssertInLog(): void
+    {
+        $this->logger->alert('test message with {name}', [ 'name' => 'foo']);
+        $this->logger->info('test message with {name}', [ 'name' => 'bar']);
+        $this->test->assertAlertInLog('test message with {name}');
+        $this->test->assertInfoInLog('test message with bar', true);
+    }
+   /**
+     * Test assertLogEmpty method
+     *
+     * @covers \DgfipSI1\testLogger\LogTestCase::assertLogEmpty
+     *
+     * @uses \DgfipSI1\testLogger\LogTestCase::__call
+     * @uses \DgfipSI1\testLogger\LogTestCase::assertInLog
+     *
+     */
+    public function testAssertLogEmpty(): void
+    {
+        $this->logger->alert('test message with {name}', [ 'name' => 'foo']);
+        $this->test->assertAlertInLog('test message with {name}');
+        $this->test->assertAlertLogEmpty();
+    }
+   /**
+     * Test assertNoMoreProdMessages method
+     *
+     * @covers \DgfipSI1\testLogger\LogTestCase::assertNoMoreProdMessages
+     *
+     * @uses \DgfipSI1\testLogger\LogTestCase::__call
+     * @uses \DgfipSI1\testLogger\LogTestCase::assertInLog
+     * @uses \DgfipSI1\testLogger\LogTestCase::assertLogEmpty
+     *
+     */
+    public function testAssertNoMoreProdMessages(): void
+    {
+        $this->logger->alert('test message with {name}', [ 'name' => 'foo']);
+        $this->test->assertAlertInLog('test message with {name}');
+        $this->logger->debug('debugging...');
+        $this->test->assertNoMoreProdMessages();
+    }
+   /**
+     * Test logReset method
+     *
+     * @covers \DgfipSI1\testLogger\LogTestCase::logReset
+     *
+     * @uses \DgfipSI1\testLogger\LogTestCase::assertNoMoreProdMessages
+     *
+     */
+    public function testLogReset(): void
+    {
+        $this->logger->alert('test message with {name}', [ 'name' => 'foo']);
+        $this->test->logReset();
+        $this->test->assertNoMoreProdMessages();
+    }
+   /**
+     * Test showNoDebugLogs method
+     *
+     * @covers \DgfipSI1\testLogger\LogTestCase::showNoDebugLogs
+     *
+     */
+    public function testShowNoDebugLogs(): void
+    {
+        $this->expectOutputRegex('/1 message.s. in logs .excluding debug./');
+        $this->logger->alert('test message with {name}', [ 'name' => 'foo']);
+        $this->logger->debug('debug message...');
+        $this->test->showNoDebugLogs();
+    }
+   /**
+     * Test showDebugLogs method
+     *
+     * @covers \DgfipSI1\testLogger\LogTestCase::showDebugLogs
+     *
+     */
+    public function testShowDebugLogs(): void
+    {
+        $this->expectOutputRegex('/1 message.s. in debug logs/');
+        $this->logger->alert('test message with {name}', [ 'name' => 'foo']);
+        $this->logger->debug('debug message...');
+        $this->test->showDebugLogs();
+    }
+}
--- tests/phpunit/src/TestLoggerTest.php
+++ tests/phpunit/src/TestLoggerTest.php
@@ -22,70 +22,97 @@ class TestLoggerTest extends TestCase
      */
     public function testsearchAndDelete(): void
     {
-        $logger = new TestLogger;
+        $logger = new TestLogger();
         $logger->alert('foo');
-        $this->assertFalse($logger->searchAndDeleteInfoRecords('foo'));
-        $this->assertTrue($logger->searchAndDeleteAlertRecords('foo'));
-        $this->assertFalse($logger->searchAndDeleteAlertRecords('foo'));
+        $this->assertFalse($logger->searchAndDeleteRecords('foo', 'info'));
+        $this->assertTrue($logger->searchAndDeleteRecords('foo', 'alert'));
+        $this->assertFalse($logger->searchAndDeleteRecords('foo', 'alert'));
     }
+    /**
+     * test Interpolation
+     *
+     * @return void
+     */
+    public function testInterpolation(): void
+    {
+        $logger = new TestLogger();
+        $logger->alert('this message is called {name}', ['name' => 'foo']);
+        $this->assertFalse($logger->searchAndDeleteRecords('is called foo', 'info'));
+        $this->assertFalse($logger->searchAndDeleteRecords('is called foo', 'alert', false));
+        $this->assertTrue($logger->searchAndDeleteRecords('is called foo', 'alert', true));
+        $this->assertFalse($logger->searchAndDeleteRecords('is called foo', 'alert', true));
+    }
+    /**
+     * test Caller Filter
+     *
+     * @return void
+     */
     public function testCallerFilter(): void
     {
         $logger = new TestLogger(['testCallerFilter']);
         $logger->alert('foo');
-        $this->assertFalse($logger->searchAndDeleteInfoRecords('foo'));
-        $this->assertTrue($logger->searchAndDeleteAlertRecords('foo'));
-        $this->assertFalse($logger->searchAndDeleteAlertRecords('foo'));
+        $this->assertFalse($logger->searchAndDeleteRecords('foo', 'info'));
+        $this->assertTrue($logger->searchAndDeleteRecords('foo', 'alert'));
+        $this->assertFalse($logger->searchAndDeleteRecords('foo', 'alert'));
 
         $logger = new TestLogger(['otherMethod']);
         $logger->alert('foo');
-        $this->assertFalse($logger->searchAndDeleteInfoRecords('foo'));
-        $this->assertFalse($logger->searchAndDeleteAlertRecords('foo'));
+        $this->assertFalse($logger->searchAndDeleteRecords('foo', 'info'));
+        $this->assertFalse($logger->searchAndDeleteRecords('foo', 'alert'));
 
         // test log from closure
         $logger = new TestLogger(['testCallerFilter']);
 
         $logger->alert('foo');
-        $alertClosure = function($message) use ($logger) {
+        $alertClosure = function ($message) use ($logger) {
             $logger->alert($message);
         };
         $alertClosure("message1");
         $alertClosure("message2");
-        $this->assertTrue($logger->searchAndDeleteAlertRecords('message1'));
-        $this->assertTrue($logger->searchAndDeleteAlertRecords('message2'));
+        $this->assertTrue($logger->searchAndDeleteRecords('message1', 'alert'));
+        $this->assertTrue($logger->searchAndDeleteRecords('message2', 'alert'));
     }
+    /**
+     * test searchRecords
+     *
+     * @return void
+     */
     public function testsearch(): void
     {
-        $logger = new TestLogger;
+        $logger = new TestLogger();
         $logger->alert('foo');
-        $this->assertFalse($logger->searchInfoRecords('foo'));
-        $this->assertTrue($logger->searchAlertRecords('foo'));
-        $this->assertTrue($logger->searchAlertRecords('foo'));
+        $this->assertFalse($logger->searchRecords('foo', 'info'));
+        $this->assertTrue($logger->searchRecords('foo', 'alert'));
+        $this->assertTrue($logger->searchRecords('foo', 'alert'));
     }
+    /**
+     * test messageList
+     *
+     * @return void
+     */
     public function testMessageList(): void
     {
-        $logger = new TestLogger;
+        $logger = new TestLogger();
         $logger->alert('foo');
         $logger->alert('bar');
         $logger->info('foobar');
 
-        $this->assertEquals(['foo', 'bar'], $logger->messageList('alert'));
-        $this->assertEquals(['foobar'], $logger->messageList('info'));
+        $this->assertEquals(["foo\n    => foo", "bar\n    => bar"], $logger->messageList('alert'));
+        $this->assertEquals(["foobar\n    => foobar"], $logger->messageList('info'));
         $this->assertEquals([], $logger->messageList('debug'));
     }
+    /**
+     * test reset
+     *
+     * @return void
+     */
     public function testReset(): void
     {
-        $logger = new TestLogger;
+        $logger = new TestLogger();
         $logger->alert('foo');
         $record = [ 'level'    => 'alert', 'message' => 'foo', 'context' => []];
         $this->assertEquals([ 'alert' => [$record]], $logger->getRecordsByLevel());
         $logger->reset();
         $this->assertEquals([], $logger->getRecordsByLevel());
     }
-    public function testBadFunctionCall(): void
-    {
-        $logger = new TestLogger;
-        $this->expectExceptionMessageMatches("/Call to undefined method/");
-        $logger->foo();
-    }
-
 }
